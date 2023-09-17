352e0213186810d2593b776d67d1753729f3a57f
Kymaeda <kyo.m.720@gmail.com>
2023-09-17 18:13:51 +0900

    とりあえずテーブル表示

--- app/javascript/components/TopContainer.tsx
+++ app/javascript/components/TopContainer.tsx
@@ -1,9 +1,52 @@
 // Hellow Worldと表示する
 import React from 'react';
 import Button from '@mui/material/Button';
+import {
+  TableContainer,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+
+type BudgetRow = {
+  name: string;
+  kind: string;
+  account: string;
+  amount: number;
+};
+
+const createData = (data: BudgetRow) => {
+  return data;
+};
+
+const rows = [createData({ name: '住居費', kind: '固定費', account: 'みずほ', amount: 87000 })];
 
 export const TopContainer = () => (
   <>
     <Button variant="contained">Hello world</Button>
+    <TableContainer sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>項目</TableCell>
+            <TableCell>種別</TableCell>
+            <TableCell>管理口座</TableCell>
+            <TableCell>金額</TableCell>
+            <TableCell>割合</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>aaaa</TableCell>
+            <TableCell>aaaa</TableCell>
+            <TableCell>aaaa</TableCell>
+            <TableCell>aaaa</TableCell>
+            <TableCell>aaaa</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   </>
 );
