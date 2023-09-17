4418dd42e1159b6a4ab010b8809c76f88b8d0a6b
Kymaeda <kyo.m.720@gmail.com>
2023-09-17 17:25:10 +0900

    material uiのコンポーネントを使用できる状態か確認

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,7 +1,12 @@
 // Hellow Worldと表示する
 import React from 'react';
+import Button from '@mui/material/Button';
+
 type propType = { name: string }
 const HellowWorld = ({ name }: propType): JSX.Element => (
-  <h1>{name}, Hellow World</h1>
+  <>
+    <h1>{name}, Hellow World</h1>
+    <Button variant="contained">Hello world</Button>
+  </>
 );
 export default HellowWorld;
