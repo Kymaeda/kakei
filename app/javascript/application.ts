0afe061771e8db8c1a234b9fbd94aa49ddc002bc
Kymaeda <kyo.m.720@gmail.com>
2023-09-17 17:45:04 +0900

    application.jsをtypescriptに変換

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import TopContainer from './components/TopContainer';
 
 // TODO: #11 で、ページ別ファイルに定義するように修正する
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 const root = createRoot(container);
 document.addEventListener('DOMContentLoaded', () => {
   root.render(<TopContainer name="Kyohei"/>)
