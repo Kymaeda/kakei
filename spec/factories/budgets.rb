a6533a7a699ef44ed87d74bf2aeca503116ca78a
Kymaeda <kyo.m.720@gmail.com>
2023-09-20 08:50:39 +0900

    spec追加

--- spec/factories/budgets.rb
+++ spec/factories/budgets.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :budget do
-    
+    started_at { Time.current.beginning_of_month }
+    finished_at { started_at.end_of_month }
+    amount { (100_000..500_000).to_a.sample }
   end
 end
--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Budget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it '開始・終了日時と金額に値がある場合、trueを返す' do
+      budget = build(:budget, started_at: 1.month.from_now, finished_at: 2.months.from_now, amount: 100_000)
+      expect(budget).to be_valid
+    end
+
+    it '開始・終了日時と金額に値がない場合、falseを返す' do
+      budget = build(:budget, started_at: nil, finished_at: nil, amount: nil)
+      expect(budget).to be_invalid
+    end
+  end
 end
