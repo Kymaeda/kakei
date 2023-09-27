7aa4d1bdc4a82f75861fe8cf78565e72c0bdf575
Kymaeda <kyo.m.720@gmail.com>
2023-09-27 20:57:09 +0900

    budget typeを作成
    ```bash
    $ rails g graphql:object Budget
    ```

---
+++ app/graphql/types/budget_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Types
+  class BudgetType < Types::BaseObject
+    field :id, ID, null: false
+    field :started_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :finished_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :amount, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
