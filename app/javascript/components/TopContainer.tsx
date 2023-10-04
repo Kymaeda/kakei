99a8c783d436fd1a9cc7363e694c675e79e91587
Kymaeda <kyo.m.720@gmail.com>
2023-10-04 09:00:58 +0900

    grapql client生成処理をserviceに切り出す

--- app/javascript/components/TopContainer.tsx
+++ app/javascript/components/TopContainer.tsx
@@ -1,29 +1,9 @@
-import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
+import { Provider } from 'urql';
+import { client } from '../services/graphql';
 import { Layout } from './Layout';
 import { BudgetTable } from './BudgetTable';
 
 export const TopContainer = () => {
-  const metaTag = document.querySelector("meta[name='csrf-token']")
-  let token: string;
-
-  // TODO: serviceに切り出す
-  if (!metaTag) {
-    throw new Error('CSRF token not found: ページを再読み込みしてください');
-  } else {
-    token = metaTag.getAttribute('content') || '';
-  }
-
-  const client = new Client({
-    url: 'http://localhost:3000/graphql',
-    // suspense: true,
-    exchanges: [cacheExchange, fetchExchange],
-    fetchOptions: () => {
-      return {
-        headers: { authorization: token ? `Bearer ${token}` : '' },
-      };
-    },
-  });
-
   return (
     <Provider value={client}>
       <Layout>
--- app/javascript/services/graphql.ts
+++ app/javascript/services/graphql.ts
@@ -1,2 +1,21 @@
-// urqlのclientを初期化する
+import { Client, cacheExchange, fetchExchange } from 'urql';
 
+const metaTag = document.querySelector("meta[name='csrf-token']");
+let token: string;
+
+if (!metaTag) {
+  throw new Error('CSRF token not found: ページを再読み込みしてください');
+} else {
+  token = metaTag.getAttribute('content') || '';
+}
+
+export const client = new Client({
+  url: 'http://localhost:3000/graphql',
+  // suspense: true,
+  exchanges: [cacheExchange, fetchExchange],
+  fetchOptions: () => {
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+    };
+  },
+});
