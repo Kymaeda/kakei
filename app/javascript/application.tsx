b992370ecf12fea6a9ed8f046dd5391b36ac52f8
Kymaeda <kyo.m.720@gmail.com>
2023-10-06 16:20:15 +0900

    fix eslint

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { TopContainer } from './components/TopContainer';
 
 // TODO: #11 で、ページ別ファイルに定義するように修正する
-const container = document.getElementById('root')!;
+const container = document.getElementById('root');
+if (!container) throw new Error('Missing root container')
+
 const root = createRoot(container);
 document.addEventListener('DOMContentLoaded', () => {
   root.render(<TopContainer />)
